c9566a12c773e088ac5377d24d6d860920188854
Joshua Parker <josh.g.parker@gmail.com>
2016-05-24 17:39:53 -0700

    update

--- Secound Attempt/Chapter 9/fillInGaps.py
+++ Secound Attempt/Chapter 9/fillInGaps.py
@@ -8,8 +8,24 @@
 '''
 import os
 
+def changeDir(folder):
+
+    os.chdir(folder)
+    foldLocation = os.path.basename(os.getcwd())
+    return
+
+def regPattern():
+    
+    filePattern = re.compile(r'.*?\d\d\d.txt') # all text before the date
+    return filePattern
+
+def searchFiles():
+    
 def main():
 
     folder = '/Users/jparker/Desktop/test'
+    changeDir(folder)
+    filePattern=regPattern()
+
 if __name__ == '__main__':
     main()

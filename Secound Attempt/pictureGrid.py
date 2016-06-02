e66dbb03feb125574af490c84f4734711e7ead62
Joshua Parker <josh.g.parker@gmail.com>
2016-06-01 17:36:58 -0700

    update

--- Secound Attempt/pictureGrid.py
+++ Secound Attempt/pictureGrid.py
@@ -18,8 +18,8 @@ def main():
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
   
-    for y in range(int(len(grid[0]))):    
-        for x in range(int(len(grid))):
+    for y in range(len(grid[0])):    
+        for x in range(len(grid)):
             print(grid[x][y], end='')
         print('\n')
             

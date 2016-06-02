3935f529b0bd80e987b23d2d442853afcac8a490
Joshua Parker <josh.g.parker@gmail.com>
2016-06-01 20:17:31 -0700

    updates

--- Secound Attempt/Chapter 11/bsModule.py
+++ Secound Attempt/Chapter 11/bsModule.py
@@ -10,23 +10,36 @@ import requests, bs4
 
 def reqPage():
     
-    res = requests.get('https://www.nostarch.com')
-    try:
-        res.raise_for_status()
-    except Exception as err:
-        print('There was a problem: ' + str(err))
-    print(type(res))
+    #res = requests.get('https://nostarch.com', verify=False)
+    res = open('example.html' )
+    #try:
+        #res.raise_for_status()
+    #except Exception as err:
+        #print('There was a problem: ' + str(err))
     return(res)
 
 def bsSoup(resobj):
-    exampleSoup = bs4.BeautifulSoup(resobj.text, "html.parser")
+    exampleSoup = bs4.BeautifulSoup(resobj.read(), "html.parser")
     elms = exampleSoup.select('p #author')
     print(type(elms))
     print(len(elms))
+    print(elms[0].getText())
+    print(elms[0])
+    print(elms[0].attrs)
+    print
+
+def bsSoup1(resobj):
+    exampleSoup = bs4.BeautifulSoup(resobj.read(), "html.parser")
+    elms = exampleSoup.select('span')[0]
+    print(type(elms))
+    print(len(elms))
+    print(elms.get('id'))
+    print(elms.attrs)
 
 def main():
     resobj=reqPage()
-    bsSoup(resobj)
+    #bsSoup(resobj)
+    bsSoup1(resobj)
 
 if __name__ == '__main__':
     main()

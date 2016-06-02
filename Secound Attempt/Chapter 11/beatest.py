a4e208694eb4ccd761ddb1b99e627b54d5d706b6
chaos777b <josh.g.parker@gmail.com>
2016-06-01 18:50:44 -0700

    updates

--- Secound Attempt/Chapter 11/beatest.py
+++ Secound Attempt/Chapter 11/beatest.py
@@ -9,12 +9,14 @@
 import requests, bs4
 
 def main():
-    res = requests.get('http://nostarch.com')
-    try:
-        res.raise_for_status();
-    except Exception as exc:
-        print('There was a problem: %s' % (exc))
-    nostartSoup=bs4.BeautifulSoup(res.text)
-    print(nostartSoup)
+    res = open('example.html' )
+    #try:
+        #res.raise_for_status();
+    #except Exception as exc:
+        #print('There was a problem: %s' % (exc))
+    nostartSoup=bs4.BeautifulSoup(res.read(),  "html.parser")
+    elems = nostartSoup.select('#author')
+    print(len(elems))
+
 if __name__ == '__main__':
     main()
